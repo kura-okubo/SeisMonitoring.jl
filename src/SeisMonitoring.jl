d81b27c858caae625001373136746cc86699f5e3
kura-okubo <kurama_okubo@fas.harvard.edu>
2020-06-29 04:15:40 -0400

    update

--- src/SeisMonitoring.jl
+++ src/SeisMonitoring.jl
@@ -44,6 +44,7 @@ include("Utils/make_slurmbatch.jl")
 # shared lib
 include("Utils/remove_nanandzerocol.jl") # used at seisstack
 include("Utils/slice_codawindow.jl") # used at seisstack
+include("Utils/energybased_slice_codawindow.jl") # used at seisstack
 include("Utils/convert_tmpfile.jl") # used at seisdownload and seisremoveeq
 include("Utils/get_noisedatafraction.jl") # used at seisdownload, seisremoveeq and seisxcorrelation
 include("Utils/split_cc.jl") # used at slice_coda_window, seismeasurement
