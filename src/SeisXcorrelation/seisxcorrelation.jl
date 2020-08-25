6861854e535086ff966e7ff170664db58197e502
kura-okubo <kurama_okubo@fas.harvard.edu>
2020-08-25 16:07:37 -0400

    update

--- src/SeisXcorrelation/seisxcorrelation.jl
+++ src/SeisXcorrelation/seisxcorrelation.jl
@@ -131,11 +131,20 @@ function seisxcorrelation(InputDict_origin::OrderedDict)
             #                                 map_compute_cc_workerpool,
             #                                 FFT1_dict, FFT2_dict, StationPairs_chunk)
             ta_2 = 0 #DEBUG
-            ta_3 = @elapsed C = pmap((x, y) -> pmaptest_1(x, y, InputDict), map_compute_cc_workerpool, StationPairs_chunk, FFT_Dict)
+            # ta_3 = @elapsed C = pmap((x, y) -> pmaptest_1(x, y, InputDict), map_compute_cc_workerpool, StationPairs_chunk, FFT_Dict)
+
+            #DEBUG: Using remote_do
+
+            # s = remotecall_fetch(f_test, wp, a)
+            ta_4 = @elapsed @sync for (i, key_station_pair) in enumerate(StationPairs_chunk)
+                # s = remotecall_fetch(f_test, wp, a[i])
+                remote_do(map_compute_cc(fft1, fft2, pair, InputDict), map_compute_cc_workerpool,
+                                FFT_Dict[netstachan1_list[i]], FFT_Dict[netstachan2_list[i]], key_station_pair)
+            end
 
             # push!(t_corr_all, mean((x->x[1]).(B)))
             push!(t_corr_all, 0) #DEBUG
-            println("time for map_fft, map_cc, testpmap = $(ta_1), $(ta_2), $(ta_3) [s]")
+            println("time for map_fft, map_cc, testpmap = $(ta_1), $(ta_2), $(ta_4) [s]")
         end
 
         ct_2 = now()
