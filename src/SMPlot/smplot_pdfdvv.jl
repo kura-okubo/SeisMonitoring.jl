3807490e1465ee00b92f16078f9c798ecbc4a32a
kura-okubo <kurama_okubo@fas.harvard.edu>
2020-11-30 00:51:04 +0900

    dvv_median

--- src/SMPlot/smplot_pdfdvv.jl
+++ src/SMPlot/smplot_pdfdvv.jl
@@ -18,7 +18,7 @@ dvv history database.
 """
 function smplot_pdfdvv(statsfile::String, fodir::String, starttime::DateTime, endtime::DateTime, time_bin_length::Real;
     cc_threshold::Float64=0.6, network_option=["all"], compontents_option::AbstractArray=["all"],
-    plotmean::Bool=true, plotmedian::Bool=true,
+    plotmean::Bool=true, plotmedian::Bool=true, plotmode::Bool=true,
     plot_maxdvv::Float64=0.05, number_of_dvvbins::Int = 30, minimum_paircount::Int=1, plottimeunit::String="day",
     figsize = (1200, 600), clims=(0.0, 0.3), xlims = [], ylims = (-0.03, 0.03), xrotation::Real=-45,
     fmt="png")
@@ -68,6 +68,7 @@ function smplot_pdfdvv(statsfile::String, fodir::String, starttime::DateTime, en
             "T" => DateTime[], #DateTime
             "dvv_mean" => Union{Float64, Missing}[],
             "dvv_median" => Union{Float64, Missing}[],
+            "dvv_mode" => Union{Float64, Missing}[],
             "dvvbins" => dvvbins,
             "pdfdvv" => Array{Union{Float64, Missing}, 2}(undef, number_of_dvvbins, 0),
             "count_pairs" => Int[],
@@ -93,6 +94,7 @@ function smplot_pdfdvv(statsfile::String, fodir::String, starttime::DateTime, en
                 # append to DvvDict
                 push!(DvvDict["dvv_mean"], missing)
                 push!(DvvDict["dvv_median"], missing)
+                push!(DvvDict["dvv_mode"], missing)
                 DvvDict["pdfdvv"] = hcat(DvvDict["pdfdvv"], fill(missing, number_of_dvvbins))
                 push!(DvvDict["count_pairs"], paircount)
                 continue;
@@ -103,8 +105,14 @@ function smplot_pdfdvv(statsfile::String, fodir::String, starttime::DateTime, en
              hn = normalize(h, mode=:probability)
              # println(hn.weights)
              # append to DvvDict
+             # compute mode at this bin
+             @show findmax(dvv_all)
+             @show dvvbins
+             dvv_mode=dvvbins[findmax(hn.weights)[2]]
+
              push!(DvvDict["dvv_mean"], Statistics.mean(dvv_all))
              push!(DvvDict["dvv_median"], Statistics.median(dvv_all))
+             push!(DvvDict["dvv_mode"], dvv_mode)
              DvvDict["pdfdvv"] = hcat(DvvDict["pdfdvv"], hn.weights)
              push!(DvvDict["count_pairs"], paircount)
         end
@@ -153,13 +161,18 @@ function smplot_pdfdvv(statsfile::String, fodir::String, starttime::DateTime, en
 
         # plot mean
         if plotmean
-            plot!(mtbins, DvvDict["dvv_mean"], size=figsize, color=:magenta, subplot=1, linewidth = 3.0,
+            plot!(mtbins, DvvDict["dvv_mean"], size=figsize, color=:cyan, subplot=1, linewidth = 3.0,
             xformatter=xformatter, xticks = xticks, label = "mean", link=:x)
         end
 
         if plotmedian
             plot!(mtbins, DvvDict["dvv_median"], size=figsize, color=:black, subplot=1, linewidth = 3.0,
             xformatter=xformatter, xticks = xticks, label = "median", link=:x)
+        end
+
+        if plotmode
+            plot!(mtbins, DvvDict["dvv_mode"], size=figsize, color=:magenta, subplot=1, linewidth = 3.0,
+            xformatter=xformatter, xticks = xticks, label = "mode", link=:x)
         end
 
         # plot only colorbar to share the x axis between contour and bar.
