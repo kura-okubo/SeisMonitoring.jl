55348fb3903b24f7d0eecaf3caeec1e7d81b4ad0
kura-okubo <kura.okubo@gmail.com>
2021-05-31 14:15:07 +0900

    update ppsd and data availability

--- src/SMPlot/smplot_ppsd.jl
+++ src/SMPlot/smplot_ppsd.jl
@@ -1,99 +1,147 @@
-using SeisIO, SeisNoise, Dates, DSP, Statistics, StatsBase, LinearAlgebra, ColorSchemes, JLD2, Plots, DataFrames, CSV
+using SeisIO, SeisNoise, Dates, DSP, Statistics, StatsBase, LinearAlgebra, ColorSchemes, JLD2, Plots, DataFrames, CSV, Measures
 using SeisMonitoring: assemble_seisdata, smooth_withfiltfilt
-"""
-    compute_psdpdf(S::SeisChannel, starttime::DateTime, endtime::DateTime)
 
-Return mean and probablistic density function of seismic noise periodgram.
+"""
+    compute_psdpdf(S::SeisChannel, fodir::String, segments_length::Real;
+	        overlap::Real=segments_length/2, smoothing_window_len::Real=7, figsize=(1200, 800),
+			fmt::String="png", xlims=(0.01, 5.0), ylims=(-200, -80), clims=(0.0, 0.3))
+
+Compute power spectrum density - probablistic density function.
+Refering process flow from obspy.signal.spectral_estimation.PPSD: https://docs.obspy.org/_modules/obspy/signal/spectral_estimation.html#PPSD
+
+# Argument
+-`S::SeisChannel`		: SeisChannel containing the waveform to compute pdfpsd.
+-`fodir::String`		: figure output directory
+-`segments_length::Real`: [s] Length of segment to compute windowed psd.
+
+# Option
+-`overlap::Real`		: [s] Length of overlap between segments to compute windowed psd.
+-`smoothing_window_len::Real` : [npts] Smoothing points.
+-`figsize=(1200, 800)`: figure size to be plotted.
+-`xlims = (0.01, 5.0)`: xlims
+-`ylims = (-200, -80)`: ylims
+-`clims = (0.0, 0.3)`: color range of pdfpsd.
+-`fmt = "png"`: figure format
+
+# Output
+-`freqvec`	: [Hz] Vector of frequency for the ppsd.
+-`bins`		: [dB] bins of amplitude for the ppsd.
+-`ppsd`		: [probability] probability density function of psd.
+-`(percentile_10, percentile_50, percentile_90)`		: Vector of 10th, 50th, 90th percentiles.
+
+# Note
+About instrumental response removal, the obspy.signal.spectral_estimation.PPSD do the removal during the computation of PPSD.
+i.e. the raw data (tr.data)
+
+
+Updated on 2021.05.25 Kurama Okubo
 """
 function compute_psdpdf(S::SeisChannel, fodir::String, segments_length::Real;
-        figsize=(1200, 800), fmt::String="png", xlims=(0.01, 5.0), ylims=(-200, -80), clims=(0.0, 0.3))
+        overlap::Real=segments_length/2, smoothing_window_len::Real=7, figsize=(1200, 800),
+		fmt::String="png", xlims=(0.01, 5.0), ylims=(-200, -80), clims=(0.0, 0.3), eps::Real=1e-24)
 
-    println("-------Plot Periodogram--------")
-    # 1. truncate with respect to the segments
-    R = RawData(S, segments_length, segments_length)
-    # 2. Preprocessing
-    detrend!(R)
-    taper!(R)
-    # 3. Compute Periodgram
-    P = DSP.periodogram(R.x, fs=R.fs)
-    # 4. Apply  smoothing NOTE:here using filtfilt instead of SeisNoise.smooth()
-    P.power[:, :] = smooth_withfiltfilt(P.power, window_len=7, window=:rect) #immutable struct of type Periodogram2 cannot be changed
-    # 5. Plot periodogram
-    p1=plot(size=figsize)
+	println("-------Compute PPSD $(S.id)--------")
 
-    tracespan = ceil(Int, size(P.power, 2)/1000) # maximum 1000 traces
+	!ispath(fodir) && mkpath(fodir)
 
-    for i = 1:tracespan:size(P.power, 2)
-        p1 = plot!(P.freq1, DSP.pow2db.(P.power[:,i]),
-            xlims=xlims, ylims=ylims, xscale=:log10, label="", color=:gray, alpha=0.4)
-    end
-    # plot median
-    p1 = plot!(P.freq1, DSP.pow2db.(mean(P.power, dims=2)), label="median", color=:red)
-    # min and max
-    p1 = plot!(P.freq1, DSP.pow2db.(minimum(P.power, dims=2)), label="minimum", color=:blue)
-    p1 = plot!(P.freq1, DSP.pow2db.(maximum(P.power, dims=2)), label="maximum", color=:purple)
-    # plot waterlevel guide
-    freqrange = [minimum(xlims); maximum(xlims)]
-    for water_level in [1e-3, 1e-4, 1e-5]
-        # compute waterlevel for deconvolution
-        wl = DSP.pow2db(water_level * mean(mean(P.power, dims=2),dims=1)[1])
-        p1 = plot!(freqrange, [wl; wl], label="water_level = $(water_level)", color=:black, linestyle=:dot)
-    end
+	R1 = RawData(S, segments_length, overlap)
+	Nseg = size(R1.x, 2)
+	Nwin = trunc(Int, segments_length*R1.fs / 4) # to make 13 segments on each trace with 75 % overlap, following obspy and McNamara and Buland (2004)
+	Noverlap = trunc(Int, 0.75 * Nwin)
+	tukey10(x) = tukey(x, 0.2) # see obspy fft_taper, which uses 10% cosine taper following McNamara and Buland (2004)
 
-    s_str = string(u2d(S.t[1,2]*1e-6))[1:19]
-    et = S.t[1,2]*1e-6 + (S.t[end,1]-1)/S.fs
-    e_str=string(u2d(et))[1:19]
-    title!("$(S.id) $(s_str) - $(e_str)")
-    xlabel!("Frequency [Hz]")
-    ylabel!("Power[10log10 (m**2/sec**2/Hz)][dB]")
+	# compute dummy psd for freq vector
+	Pseg_dum = welch_pgram(R1.x[:, 1], Nwin, Noverlap, onesided=true, fs=R1.fs, window=tukey10)
+	freqvec = Pseg_dum.freq
+	ω = 2.0 * π .* freqvec
+	bins = -200:1.0:-80 # Fix bins for simplicity
 
-    !ispath(fodir) && mkdir(fodir)
-    savefig(p1, fodir*"/periodogram__$(S.id)__$(s_str)__$(e_str).$(fmt)")
+	p_all = zeros(Float64, length(Pseg_dum.power), Nseg)
+	ppsd =  zeros(Float64, length(bins)-1, length(freqvec)) #(number of bins, frequency)
 
-    # compute probability density function of power spectral density (PDFPSD)
-    # following McNamara and Buland (2004), using 1-db binned  between -200:-80 db histogram to obtain pdf.
-    println("-------Plot PDFPSD--------")
+	# compute periodogram using welch method
+	t2 = @elapsed for i in 1:Nseg
+		Pseg = welch_pgram(R1.x[:, i], Nwin, Noverlap, onesided=true, fs=R1.fs, window=tukey10)
+		#NOTE: Convert from velocity to acceleration power
+		if S.units == "m/s"
+			p_all[:, i] = Pseg.power .* ω.^2 # note that this is power/Hz normalized by window when using oneside=true
+		elseif S.units == "m/s/s"
+			p_all[:, i] = Pseg.power
+		elseif S.units == "m"
+			p_all[:, i] = Pseg.power .* ω.^4 # displacement -> acceleration
+		else
+			error("Unit of data $(S.units) is unknown.")
+		end
+	end
 
-    bins = -200:1.0:-80
-    rfft_halflen =  round(Int, size(P.power,1)/2) # assuming real signal, compute half of the fft.
-    ppsd = Array{Float64, 2}(undef, length(bins)-1, rfft_halflen) #(number of bins, frequency)
+	p_all[:, :] = smooth_withfiltfilt(p_all, window_len=smoothing_window_len, window=:rect) #immutable struct of type Periodogram2 cannot be changed
 
-    for i =1:rfft_halflen
-         h = StatsBase.fit(Histogram, DSP.pow2db.(P.power[i,:]), bins)
-         hn = normalize(h, mode=:pdf)
-         ppsd[:, i] = hn.weights
-    end
+	# Compute 10th, 50th and 90th percentiles
+	percentile_10 = zeros(length(freqvec), 1)
+	percentile_50 = zeros(length(freqvec), 1)
+	percentile_90 = zeros(length(freqvec), 1)
 
-    # compute logspace bins in frequency to better plot contourf of probability density function
-    Nplotfreq = 1000
-    xq = 10 .^(range(log10(xlims[1]), stop=log10(xlims[2]), length=Nplotfreq))
-    ixq = []
-    for i = 1:length(xq)
-        push!(ixq, findfirst(x -> x >= xq[i], P.freq1))
-    end
-    unique!(ixq)
+	for i =1:length(freqvec)
+		p10 = StatsBase.percentile(p_all[i, :], 10)
+		p50 = StatsBase.percentile(p_all[i, :], 50)
+		p90 = StatsBase.percentile(p_all[i, :], 90)
+		percentile_10[i] = p10 > eps ? DSP.pow2db(p10) : NaN
+		percentile_50[i] = p50 > eps ? DSP.pow2db(p50) : NaN
+		percentile_90[i] = p90 > eps ? DSP.pow2db(p90) : NaN
+		# check if 50th percentile == median
+		if ~isnan(percentile_50[i])
+			@assert percentile_50[i] == DSP.pow2db.(Statistics.median(p_all[i, :]))
+		end
+	end
 
-    # load pqlx colormap
-    c_pqlx = jldopen(joinpath(splitdir(pathof(SeisMonitoring))[1], "SMPlot/smplot_pqlx.jld2"), "r") do fi; fi["pqlx.colors"]; end
-    loadcolorscheme(:pqlx, c_pqlx, "pqlx color", "for pdfpsd plot")
-    #===#
+	# compute histogram on each frequency to obtain pdf
+	for i =1:length(freqvec)
+		psd_tmp = p_all[i,:]
+		filter!(x -> x > eps, psd_tmp) # filter out the too small powers
+		h = StatsBase.fit(Histogram, DSP.pow2db.(psd_tmp), bins)
+		hn = normalize(h, mode=:pdf)
+		ppsd[:, i] = hn.weights
+	end
 
-    # NOTE: heatmap is not clean to plot
-    # heatmap(P.freq1[ixq], collect(bins)[1:end-1], ppsd[:, ixq],
-    #         xscale=:log10, clims=clims, xlims=(0.01,5.0), ylims=(-200,-80),
-    #         color=heatmapcolor)
+	# plot(bins[1:end-1], ppsd[:, 1:100:end])
 
-    contourf(P.freq1[ixq], collect(bins)[1:end-1], ppsd[:, ixq], color=:pqlx,
-    levels=30, clims=clims, xscale=:log10, xlims=xlims, ylims=ylims, linewidth=0.0,
-    size=figsize, colorbar_title="Probability")
+	# test plot
+	xlims=(0.01, 5.0)
+	ylims=(-200, -50)
 
-    title!("$(S.id) $(s_str) - $(e_str)")
-    xlabel!("Frequency [Hz]")
-    ylabel!("Power[10log10 (m**2/sec**2/Hz)][dB]")
-    savefig(fodir*"/pdfpsd__$(S.id)__$(s_str)__$(e_str).$(fmt)")
+	Nplotfreq = 100
+	xq = 10 .^(range(log10(xlims[1]), stop=log10(xlims[2]), length=Nplotfreq)) # logalithmically uniform octave space
+	ixq = []
+	for i = 1:length(xq)
+	push!(ixq, findfirst(x -> x >= xq[i], freqvec))
+	end
+	unique!(ixq)
 
-    println("smplot_ppsd is successfully done.")
+	# load pqlx colormap
+	c_pqlx = jldopen(joinpath(splitdir(pathof(SeisMonitoring))[1], "SMPlot/smplot_pqlx.jld2"), "r") do fi; fi["pqlx.colors"]; end
+	~haskey(colorschemes, :pqlx) && loadcolorscheme(:pqlx, c_pqlx, "pqlx color", "for pdfpsd plot");
+	#===#
 
+	t3 = @elapsed heatmap(freqvec[ixq], bins[1:end-1], ppsd[:, ixq], clims=(0.0, 0.3), color=:pqlx,
+	 xlims=xlims, ylims=ylims, framestyle = :box, margin = 15mm, grid=true,
+	 xticks=([1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3], [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3]),
+	levels=30, xscale=:log10, linewidth=0.0, size=(800,600), colorbar_title="Probability",
+	)
+
+	#plot percentile
+	plot!(freqvec[ixq], percentile_10[ixq], linestyle=:solid, linewidth = 2,linecolor = :cyan, label="10th%", legend=:topright)
+	plot!(freqvec[ixq], percentile_50[ixq], linestyle=:solid, linewidth = 2,linecolor = :red, label="median")
+	plot!(freqvec[ixq], percentile_90[ixq], linestyle=:solid, linewidth = 2,linecolor = :orange, label="90th%")
+
+	s_str = string(u2d(S.t[1,2]*1e-6))[1:19]
+	et = S.t[1,2]*1e-6 + (S.t[end,1]-1)/S.fs
+	e_str=string(u2d(et))[1:19]
+	title!("$(S.id) $(s_str) - $(e_str)")
+	xlabel!("Frequency [Hz]")
+	ylabel!("Power[10log10 (m**2/sec**4/Hz)][dB]")
+	savefig(fodir*"/pdfpsd__$(S.id)__$(s_str)__$(e_str).$(fmt)")
+	println(t2, t3)
+    return freqvec, bins, ppsd, ixq, (percentile_10, percentile_50, percentile_90)
 
 end
 
@@ -107,28 +155,38 @@ Plot the probability density function of power spectral density (PSDPDF) followi
 
 # Argument
 -`fidir::String`: absolute/relative path to SeisMonitoring.jl format data. (e.g. "OUTPUT/RawData.jld2")
--`fodir::String`: figure names. (e.g. "fig1" produces "periodgram_fig1.fmt" and "pdfpsd_fig1.fmt")
--`channel::String`: station, channel name (e.g. BP.CCRB..BP1)
+-`fodir::String`:
+-`netstalocchan::String`: station, channel name (e.g. BP.CCRB..BP1)
 -`starttime::DateTime`: start time of continuous data.
 -`endtime::DateTime`: end time of continuous data.
 -`segments_length::Real=3600`: [second] segments length to compute power spectral density. 3600sec is used in McNamara and Buland (2004).
 
+
+# Argument
+-`S::SeisChannel`		: SeisChannel containing the waveform to compute pdfpsd.
+-`fodir::String`		: figure output directory
+-`segments_length::Real`: [s] Length of segment to compute windowed psd.
+
 # Option
+-`overlap::Real`		: [s] Length of overlap between segments to compute windowed psd.
+-`smoothing_window_len::Real` : [npts] Smoothing points.
 -`figsize=(1200, 800)`: figure size to be plotted.
 -`xlims = (0.01, 5.0)`: xlims
 -`ylims = (-200, -80)`: ylims
 -`clims = (0.0, 0.3)`: color range of pdfpsd.
 -`fmt = "png"`: figure format
 """
-function smplot_pdfpsd(fidir::String, fodir::String, channel::String, starttime::DateTime,
-    endtime::DateTime, segments_length::Real=3600; figsize=(1200, 800), xlims = (0.01, 5.0),
+function smplot_pdfpsd(fidir::String, fodir::String, netstalocchan::String, starttime::DateTime,
+    endtime::DateTime, segments_length::Real=3600; overlap::Real=segments_length/2, smoothing_window_len::Real=7, figsize=(1200, 800), xlims = (0.01, 5.0),
     ylims = (-200, -80), clims = (0.0, 0.3), fmt = "png")
 
     # fi = jldopen(fidir, "r")
-    S1 = assemble_seisdata(channel, fidir, starttime, endtime, data_contents_fraction=0.0)
+    t1 = @elapsed S1 = assemble_seisdata(netstalocchan, fidir, starttime, endtime, data_contents_fraction=0.0)
+	println(t1)
 
-    compute_psdpdf(S1, fodir, segments_length, figsize=figsize, fmt=fmt, xlims=xlims, ylims=ylims, clims=clims)
-    # close(fi)
+	compute_psdpdf(S1, fodir, segments_length, overlap=overlap, smoothing_window_len=smoothing_window_len,
+	       figsize=figsize, fmt=fmt, xlims=xlims, ylims=ylims, clims=clims)
+
 end
 
 #===NOTE: if the pdlx.jld2 is missing, run the script below to make colorschemes from (r,g,b) text file===#
---
+++ src/SMStats/smstats_dataavailability.jl
@@ -0,0 +1,113 @@
+using SeisIO, CSV
+"""
+    smstats_dataavailability(fidir::String,foname::String,
+                            starttime::DateTime,
+                            endtime::DateTime;
+                            network::Union{String, AbstractArray}=["all"]
+                            )
+Compute noise data fraction as data abvailability.
+This function is extended from smplot_noiseavailability.jl
+
+# Argument
+- `fidir::String`: absolute/relative path to seismicdata with seisio format.
+- `foname::String`: figure and csv output directory.
+- `starttime::DateTime`: starttime to be plotted.
+- `endtime::DateTime`: endtime to be plotted.
+- `network::Union{String, AbstractArray}=["all"]`: Array of network to be plotted. (e.g. ["all"], or ["BP, "NC"])
+"""
+function smstats_dataavailability(fidir::String,foname::String,
+    starttime::DateTime,
+    endtime::DateTime;
+    network::Union{String, AbstractArray}=["all"])
+
+    typeof(network) == String && (network = [network])
+
+    PlotDict=Dict(
+    "fidir" => fidir,
+    "foname" => foname,
+    "starttime" => starttime,
+    "endtime" => endtime,
+    "network" => network
+    )
+
+    println("***************************************")
+    println("Plot Noise Availability")
+    println("starttime    = $(starttime)")
+    println("endtime      = $(endtime)")
+    println("network      = $(network)")
+    println("***************************************\n")
+
+	stations = String[]
+	for (root, dirs, files) in walkdir(PlotDict["fidir"])
+       for file in files
+		   fi = joinpath(root, file)
+		   (split(fi, ".")[end] == "seisio") && push!(stations, fi)# filter if it is .seisio
+       end
+    end
+
+    # filter the network
+    "all" ∉ network && filter!(x -> split(splitdir(x)[2], ".")[1] ∈ network, stations)
+
+    # parallelize with keys in Rawdata.jld2 i.e. stations
+    println("-------START Getting Noise Data Fraction--------")
+
+    df_mapped = pmap(x -> map_getnoisedatafraction(x, PlotDict), stations)
+
+    df_all = DataFrame(station=String[], date = String[], data_fraction=Float64[], removal_fraction=Float64[])
+
+    for df in df_mapped
+        !isnothing(df) && append!(df_all, df);
+    end
+
+    # output csvfile
+
+    CSV.write(foname*".csv", df_all)
+
+    println("smstats_dataavailability is successfully done.")
+
+    return nothing
+end
+
+"""
+map_getnoisedatafraction(station::String, InputDict::Dict)
+
+Return DataFrame containing stationchannel, date and noise data fraction.
+"""
+function map_getnoisedatafraction(stationpath::String, PlotDict::Dict)
+
+    fidir, fikey = splitdir(stationpath)
+
+	# process only .seisio file
+	if split(fikey, ".")[end] != "seisio"
+		return nothing
+	end
+
+    # println("start process on $(fikey)")
+
+    df_station = DataFrame(station=String[], date = String[], data_fraction=Float64[], removal_fraction=Float64[])
+
+    # fi = jldopen(PlotDict["filename"], "r")
+
+    # for fikey in keys(fi["Waveforms/$(station)"])
+    file_st, file_et = DateTime.(split(fikey, "__")[2:3])
+
+    if !(PlotDict["starttime"] >= file_et || PlotDict["endtime"] <= file_st)
+        # this file has overlap with the target timewindow [starttime, endtime]
+		# S1 = fi["Waveforms/$(station)/$(fikey)"]
+		S1 = rseis(stationpath)[1]
+        centraltime = string(u2d((d2u(file_st) + d2u(file_et))/2))
+        data_fraction = S1.misc["data_fraction"]
+
+		if haskey(S1.misc, "removal_fraction")
+			# this data is for remove seisEQ
+			removal_fraction = S1.misc["removal_fraction"]
+		else
+			removal_fraction = 0.0 # raw data (no removal)
+		end
+
+        push!(df_station, (S1.id, centraltime, data_fraction, removal_fraction))
+    end
+
+    return df_station
+
+end
