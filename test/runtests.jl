a9c8e6cfcc2e1fea8a89b412b5be59c3a0e99874
kura-okubo <kura.okubo@gmail.com>
2023-09-04 19:42:57 +0900

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,5 @@
 using Test
 using SeisMonitoring
 
-
-# @testset "SeisMonitoring.jl" begin
-#     # Write your own tests here.
-#
-#     @test  typeof("Test script will be implemented.") == String
-#
-# end
-
 include("dvvmeasurement_validation.jl")
-# include("run_seismonitoring_process.jl")
+include("run_seismonitoring_process.jl")
