e84c26149a9edbdf1e36cdcf83228435777d805c
kura-okubo <kura.okubo@gmail.com>
2023-09-05 22:15:20 +0900

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Test
 using SeisMonitoring
 
+@testset "SeisMonitoring test" begin
+
 include("dvvmeasurement_validation.jl")
 include("run_seismonitoring_process.jl")
 include("auxiliary_test.jl")
+
+end
